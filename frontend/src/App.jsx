943ea84c466e51d7443bd3c5dffbf8e468e6face
Paulo Nascimiento <nascimientpaulo@meta.com>
2025-06-20 11:57:30 -0700

    added basic front-end, starting with private game logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hasPGBeenClicked, setHasPGBeenClicked] = useState(false) 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>GeoTug</h1>
+      <h2>A best-of-7 tug-of-war flag guessing game</h2>
+      <div className="button-wrapper">
+        <button disabled>Matchmaking</button>
+        <div className="private-game-button-wrapper">
+          <button 
+            type="button" 
+            onClick={() => setHasPGBeenClicked(!hasPGBeenClicked)}>
+              Private Game
+          </button>
+            {hasPGBeenClicked && (
+              <div className="create-join-wrapper">
+                <button>Create Game</button>
+                <button>Join Game</button>
+              </div>
+            )}
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
